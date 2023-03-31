38d2201d5abc6e002b03b67029f2aae5283ce334
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-03-31 21:40:49 +0200

    Update ejercicio5.sql

--- ejercicio5.sql
+++ ejercicio5.sql
@@ -36,34 +36,48 @@ CREATE TABLE reservas (
 
 INSERT INTO facultades (nombre) VALUES 
 	('Universidad de Girona'), 
-    ('Universidad Rovira i Virgili'), 
-    ('Universidad Politecnica de Barcelona'), 
-    ('Universidad Pompeu Fabra'), 
-    ('Universidad de Barcelona');
+    	('Universidad Rovira i Virgili'), 
+    	('Universidad Politecnica de Barcelona'), 
+    	('Universidad Pompeu Fabra'), 
+	('Universidad de Lleida'), 
+	('Universidad de Vic'), 
+	('Universidad de Online de Catalunya'), 
+	('Universidad Autonoma de Barcelona'), 
+	('Universidad de Andorra'), 
+    	('Universidad de Barcelona');
 
 INSERT INTO investigadores VALUES
 	('75132883', 'Simona Rosado', 1),
 	('48784301', 'Tomas Pons', 2),
 	('53188299', 'Maria Gamero', 3),
-	('37902631', 'Miriam Estrada', 4),
+	('37902631', 'Miriam Estrada', 7),
 	('87112941', 'Amadeo Sabater', 3),
 	('92069921', 'Elisabeth Trillo', 4),
-	('11267069', 'Ismael Pazos', 2),
-	('32712491', 'Marta Alvarez', 1);
+	('11267069', 'Ismael Pazos', 6),
+	('32712491', 'Marta Alvarez', 8),
+	('75132883', 'Pablo Mora', 9),
+	('75132883', 'Jose Luis Bize', 7);
 
 INSERT INTO equipos VALUES 
 	('123','Agitador', 4),
-	('324','Microscopio', 2),
+	('324','Microscopio optico', 2),
 	('554','Criostato', 3),
 	('678','Campana extractora', 1),
 	('973','Destilador', 3),
-	('258','Microscopio', 1),
-	('394','Espectrofotometro', 2),
+	('258','Microscopio electronico', 6),
+	('885','Medidor fotovoltaico', 7),
+	('113','Ordenador', 9),
+	('394','Espectrofotometro', 8),
 	('457','Bomba de vacio', 4);
 
 INSERT INTO reservas VALUES 
 	('75132883', '678', '2023-04-14', '2023-04-16'),
 	('92069921', '457', '2023-06-30', '2023-07-02'),
 	('87112941', '973', '2023-05-21', '2023-05-29'),
-	('32712491', '258', '2023-06-01', '2023-06-05'),
+	('32712491', '324', '2023-06-01', '2023-06-05'),
+	('32712491', '258', '2023-07-07', '2023-07-09'),
+	('32712491', '885', '2023-08-03', '2023-08-09'),
+	('32712491', '258', '2023-12-01', '2023-12-08'),
+	('32712491', '113', '2023-10-10', '2023-10-25'),
+	('32712491', '885', '2023-06-01', '2023-06-15'),
 	('11267069', '394', '2023-04-26', '2023-04-28');
